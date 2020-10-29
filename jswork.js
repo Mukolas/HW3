8e567eb44af4b9cbc5473ec6d304f78b118f0b11
Mukolas <72994633+Mukolas@users.noreply.github.com>
2020-10-29 19:04:19 +0200

    Add files via upload

---
+++ jswork.js
@@ -0,0 +1,154 @@
+// Перше завдання
+// let a= "hello";
+// let b= "uwu";
+// let c= "com";
+// let d= "ua";
+// let e= 1;
+// let f= 10;
+// let g= -999;
+// let h= 123;
+// let j= 3.14;
+// let k= 2.7;
+// let l= 16;
+// let r= true; 
+// let t= false;
+//  console.log(a,b,c,d,e,f,g,h,j,k,l,r,t);
+//  document.write(a+' '+b+' '+c+' '+d+' '+e+' '+f+' '+g+' '+h+' '+j+' '+k+' '+l+' '+r+' '+t);
+//  alert(a+' '+b+' '+c+' '+d+' '+e+' '+f+' '+g+' '+h+' '+j+' '+k+' '+l+' '+r+' '+t);
+//
+//Zavdannya2
+//
+// let a= "hellow";
+// let b= "uwuw";
+// let c= "comw";
+// let d= "uaw";
+// let e= 10;
+// let f= 100;
+// let g= -9990;
+// let h= 1230;
+// let j= 3.140;
+// let k= 2.70;
+// let l= 160;
+// let r= false; 
+// let t= true;
+//  console.log(a,b,c,d,e,f,g,h,j,k,l,r,t);
+//  document.write(a+' '+b+' '+c+' '+d+' '+e+' '+f+' '+g+' '+h+' '+j+' '+k+' '+l+' '+r+' '+t);
+//  alert(a+' '+b+' '+c+' '+d+' '+e+' '+f+' '+g+' '+h+' '+j+' '+k+' '+l+' '+r+' '+t);
+//
+//Zavdannya3
+//
+// let imya = "Mykola";
+// let prizvishche = "Sozanskiy";
+// let poBatkovi = "Yaroslavovich";
+// let numberOne =1;
+// let numberTwo =2;
+// let numberThree =3;
+// console.log(imya+' '+prizvishche+' '+poBatkovi+' '+numberOne+' '+numberTwo+' '+numberThree);
+// document.write(imya+' '+prizvishche+' '+poBatkovi+' '+numberOne+' '+numberTwo+' '+numberThree);
+// alert(imya+' '+prizvishche+' '+poBatkovi+' '+numberOne+' '+numberTwo+' '+numberThree);
+//
+//Zavdannya4
+//
+// let imya = prompt("Vvedit' svoye imya:");
+// let prizvishche = prompt("Vvedit' svoye prizvishche:");
+// let poBatkovi = prompt("Vvedit' svoye imya po bat'kowi:");
+// console.log(imya+' '+prizvishche+' '+poBatkovi);
+// document.write(imya+' '+prizvishche+' '+poBatkovi);
+// alert(imya+' '+prizvishche+' '+poBatkovi);
+//
+//Zavdannya5
+//
+// let imya = prompt("Vvedit' svoye imya:");
+// let prizvishche = prompt("Vvedit' svoye prizvishche:");
+// let poBatkovi = prompt("Vvedit' svoye imya po bat'kowi:");
+// let person = imya+' '+prizvishche+' '+poBatkovi;
+// document.write(person);
+//
+//Zavdannya6
+//
+// let imya = prompt("Vvedit' svoye imya:");
+// let prizvishche = prompt("Vvedit' svoye prizvishche:");
+// let poBatkovi = prompt("Vvedit' svoye imya po bat'kowi:");
+// let imyaMami = prompt("Vvedit' imya Mami:");
+// let prizvishcheMami = prompt("Vvedit' prizvishche Mami:");
+// let poBatkoviMami = prompt("Vvedit' imya po bat'kowi Mami:");
+// let imyaBatka = prompt("Vvedit' imya Bat'ka:");
+// let prizvishcheBatka = prompt("Vvedit' prizvishche Bat'ka:");
+// let poBatkoviBatka = prompt("Vvedit' imya po bat'kowi Bat'ka:");
+// let imyaSestry = prompt("Vvedit' imya Sestry:");
+// let prizvishcheSestry = prompt("Vvedit' prizvishche Sestry:");
+// let poBatkoviSestry = prompt("Vvedit' imya po bat'kowi Sestry:");
+
+// let person = imya+' '+prizvishche+' '+poBatkovi;
+// let personMami = imyaMami+' '+prizvishcheMami+' '+poBatkoviMami;
+// let personBatka = imyaBatka+' '+prizvishcheBatka+' '+poBatkoviBatka;
+// let personSestry = imyaSestry+' '+prizvishcheSestry+' '+poBatkoviSestry;
+
+// document.write("Washe FIO: "+ person+'<br>');
+// document.write("FIO Mami: "+personMami+'<br>');
+// document.write("FIO Bat'ka: "+personBatka+'<br>');
+// document.write("FIO Sestry: "+personSestry+'<br>'   );
+//
+//Zavdannya7
+//
+// let chisloOne= prompt("vvedit chislo 1"); 
+// let chisloTwo= prompt("vvedit chislo 2"); 
+// let chisloThree= prompt("vvedit chislo 3"); 
+// console.log(+chisloOne);
+// console.log(+chisloTwo);
+// console.log(+chisloThree);
+//
+//Zavdannya8
+//
+// let chisloOne= prompt("vvedit chislo 1"); 
+// let chisloTwo= prompt("vvedit chislo 2"); 
+// let chisloThree= prompt("vvedit chislo 3"); 
+// let chisloFour= prompt("vvedit chislo 4");
+// let intone = parseInt(chisloOne);
+// let inttwo = parseInt(chisloTwo);
+// let intthree= parseInt(chisloThree);
+// let intfour= parseInt(chisloFour);
+// let result = intone+inttwo+intthree+intfour;
+// console.log(result);
+//
+//Zavdannya9
+//
+// let chisloOne= prompt("vvedit chislo 1"); 
+// let chisloTwo= prompt("vvedit chislo 2"); 
+// let chisloThree= prompt("vvedit chislo 3");
+// let Fone = parseFloat(chisloOne);
+// let Ftwo = parseFloat(chisloTwo);
+// let Fthree= parseFloat(chisloThree);
+// let result = Fone+Ftwo+Fthree;
+// console.log(result);
+//
+//Zavdannya10
+//
+// let chisloOne= prompt("vvedit chislo 1"); 
+// let chisloTwo= prompt("vvedit chislo 2"); 
+// let chisloThree= prompt("vvedit chislo 3");
+// let roundOne= Math.round(chisloOne);
+// let roundTwo= Math.round(chisloTwo);
+// let roundThree= Math.round(chisloThree);
+// let result = roundOne+roundTwo+roundThree;
+// console.log(result);
+//
+//Zavdannya11
+//
+// let chisloOne= prompt("vvedit chislo 1"); 
+// let chisloTwo= prompt("vvedit chislo 2"); 
+// let intOne= parseInt(chisloOne);
+// let intTwo=parseInt(chisloTwo);
+// let stepin= Math.pow(intOne,intTwo);
+// console.log(stepin);
+//
+//Zavdannya12
+//
+let a = 100; 
+let b = '100'; 
+let c = true; 
+let d = undefined;
+console.log(a+' is '+typeof a);
+console.log(b+' is '+typeof b);
+console.log(c+' is '+typeof c);
+console.log(d+' is '+typeof d);
